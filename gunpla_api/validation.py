b5e50cc7a82c884dba83365bdab585a1f74717c2
jnhustin <jnhustin@gmail.com>
2020-07-26 14:44:12 -0700

    added a dict safe_get

--- gunpla_api/validation.py
+++ gunpla_api/validation.py
@@ -7,12 +7,14 @@ logger  =  Logger().get_logger()
 class Validation():
 
 
-  def get_json_field(self, field, json):
+  def get_json_field(self, field, json, optional=False):
+    if optional:
+      return json.get(field)
     try:
       return json[field]
     except:
       logger.exception('[get_json_field] error')
-      raise BadRequestException
+      raise BadRequestException(f'missing field: {field}')
 
 
   def get_query_param(self, field, params):
