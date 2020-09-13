ae3e03725663d3ba6c587bc107ced3ea123c2098
jnhustin <jnhustin@gmail.com>
2020-09-13 11:06:48 -0700

    remove unused files

--- scraper/run.py
+++ scraper/run.py
@@ -16,7 +16,7 @@ from bs4 import BeautifulSoup
     'release_date'  :  '',
     'images'        :  {
       'main_image' :  '',
-      'box_art' '',
+      'box_art': '',
       gallery_images :  [ '', '', ...]
     }
   }
@@ -61,54 +61,9 @@ site_urls = {
   'gundam_planet_pg' :  'https://www.gundamplanet.com/gundam/perfect-grade.html?product_list_limit=108',
   'gundam_planet_mg' :  'https://www.gundamplanet.com/gundam/master-grade.html?product_list_limit=108',
   'gundam_planet_hg' :  'https://www.gundamplanet.com/gundam/high-grade.html?product_list_limit=108',
-  'gundam_planet_pg' :  'https://www.gundamplanet.com/gundam/perfect-grade.html',
   'gundam_planet_og' :  'https://www.gundamplanet.com/gundam/other-grade.html?product_list_limit=108',
   'gundam_planet_sd' :  'https://www.gundamplanet.com/gundam/sd-gundam.html?product_list_limit=108',
 }
-
-page = requests.get(site_urls['gundam_planet_mg'])
-with open('mg.html', 'w') as f:
-  f.write(page.text)
-
-
-
-
-# soup = BeautifulSoup(page.content, 'html.parser')
-
-# get all links + base image from hg, pg, mg list
-  # go to base page
-  # loop through pages
-  # grab link
-  # grab image
-# loop through each
-  # grab data set
-
-
-
-# gundam planet
-# grab the image + all links into the product
-# TODO - figure out pagination
-  # find ul with class = 'items pages-items
-    # grab all hrefs from child.ul.li.a tags
-
-
-
-# ol. class = 'products list items product-items'
-  # child.div (class = 'products list items product-items')
-    # details page = child.a.href
-    # image = child.a.image.src (class = 'photo image')
-
-# print('soup:', soup)
-# table = soup.findAll('li', {'class': 'item product product-item blue_wrap'})
-# # print('table:', len(tabxle))
-
-# for model in table:
-#   link_tag = model.find('a', {'class': 'product-item-link'})
-#   model_name = link_tag.text
-#   model_link = link_tag['href']
-#   print(f'{model_name}: {model_link}')
-
-
 
 
 
